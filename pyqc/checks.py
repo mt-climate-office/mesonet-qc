c5a24e1c2e39bef1c353871c7ac935098210b5d1
colinbrust <colin.brust@gmail.com>
2022-12-01 17:05:19 -0700

    Generalized method for QA/QC checking with Pandas DataFramse.

--- pyqc/checks.py
+++ pyqc/checks.py
@@ -1,21 +1,133 @@
 import numpy as np
+import pandas as pd
 from typing import List
+
+from columns import Columns
 
 Numeric = float | int | np.number
 
 def check_range(x: Numeric, low: Numeric, high: Numeric) -> int:
+    """_summary_
+
+    Args:
+        x (Numeric): _description_
+        low (Numeric): _description_
+        high (Numeric): _description_
+
+    Returns:
+        int: _description_
+    """
     if x > low and x < high:
         return 0
     return 1
 
 
+def check_range_pd(dat: pd.DataFrame, columns: Columns, **kwargs) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        dat (pd.DataFrame): _description_
+        columns (Columns): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    dat = dat.assign(
+        qa_range = dat[columns.compare_col].between(dat[columns.min_col], dat[columns.max_col], inclusive="both")
+    )
+    dat = dat.assign(
+        qa_range = (-dat['qa_range']).astype(int)
+    )
+    return dat
+
+
 def check_step(x: Numeric, prev: Numeric, threshold: Numeric) -> int:
+    """_summary_
+
+    Args:
+        x (Numeric): _description_
+        prev (Numeric): _description_
+        threshold (Numeric): _description_
+
+    Returns:
+        int: _description_
+    """
     if abs(prev - x) < threshold:
         return 0
     return 1
 
 
+def check_step_pd(dat: pd.DataFrame, columns: Columns, **kwargs) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        dat (pd.DataFrame): _description_
+        columns (Columns): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    dat = dat.assign(
+        diff = abs(dat[columns.compare_col].rolling(2).apply(lambda x: x.iloc[1] - x.iloc[0]))
+    )
+    dat = dat[~dat['diff'].isna()]
+    dat = dat.assign(
+        qa_step = (~(dat['diff'] < dat[columns.step_col])).astype(int)
+    )
+    dat = dat.drop(columns=["diff"])
+    return dat
+
+
+
 def check_variance(x: np.ndarray | List[Numeric], threshold: Numeric) -> int:
+    """_summary_
+
+    Args:
+        x (np.ndarray | List[Numeric]): _description_
+        threshold (Numeric): _description_
+
+    Returns:
+        int: _description_
+    """
     if np.std(x) > threshold:
         return 0
     return 1
+
+
+def check_variance_pd(dat: pd.DataFrame, columns: Columns, **kwargs: pd.DataFrame) -> pd.DataFrame:
+    """Check that the standard deviation of observations over the course of a day are above a
+    specified threshold.
+
+    Args:
+        dat (pd.DataFrame): A DataFrame of observations and threshold values for the test.
+        columns (Columns): A mapping of columns to use in the calculation. 
+        **kwargs (pd.DataFrame): Optional - A DataFrame of daily variance. 
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    if "variance_df" in kwargs:
+        dat = dat.merge(sd, on=[columns.elem_col, "date"], how="left")
+    else: 
+        dat = dat.assign(
+            datetime = pd.to_datetime(dat[columns.dt_col])
+        )
+        
+        sd = dat.groupby([pd.Grouper(key=columns.dt_col, freq="1D"), columns.elem_col])[columns.compare_col].std().reset_index()
+        sd = sd.assign(
+            date = sd[columns.dt_col].dt.date
+        )
+        dat = dat.assign(
+            date = dat[columns.dt_col].dt.date
+        )
+        sd = sd[["date", columns.elem_col, columns.compare_col]]
+        sd.columns = ["date", columns.elem_col, "sd"]
+
+        dat = dat.merge(sd, on=[columns.elem_col, "date"], how="left")
+
+    dat = dat.assign(
+        qa_delta = (~(dat['sd'] > dat[columns.delta_col])).astype(int)
+    )
+
+    dat = dat.drop(columns=["sd"])
+    return dat
--- pyqc/process.py
+++ pyqc/process.py
@@ -1,19 +1,44 @@
 import pandas as pd
+import checks as ck
+from columns import Columns
 
+from typing import Callable
 
-def check_observations(dat: pd.DataFrame, thresholds:pd.DataFrame) -> pd.DataFrame:
+def check_observations(
+    dat: pd.DataFrame, thresholds:pd.DataFrame, columns: Columns, *checks: Callable, variance_df: pd.DataFrame=None
+) -> pd.DataFrame:
     """Given a long-formatted dataframe of observations and a dataframe of qa/qc check thresholds for 
     each element in the observations, perform all appropriate qa/qc checks.
 
     Args:
         dat (pd.DataFrame): A long-formatted dataframe of wether observations with the following columns:
-        `station`, `datetime`, `element`, `value`, `units`.
-        thresholds (pd.DataFrame): A dataframe of thresholds with the following columns:
-        `element`, `range_min`, `range_max`, `step_size`, `persistence_delta`
+        `station`, `datetime`, `element`, `value`.
+        thresholds (pd.DataFrame): A dataframe of thresholds for different QA/QC tests. Should have
+        columns outlined in the `Columns` class. 
+        columns (Columns): A Class mapping the column names of `dat` and `thresholds` to those used 
+        in the QA/QC checking functions. 
+        *checks (Callable): QA/QC functions from `check.py` that will be used to check the observations.
+        variance_df (pd.DataFrame): A DataFrame with columns `element` and `sd` and `date` that gives the 
+        daily standard deviation of daily observations of a given element. If provided it will be used
+        for the persistence delta QA/QC check. If left blank, it is assumed that sub-hourly values are provided
+        for an entire day in order to calculate the sd. 
 
     Returns:
-        pd.DataFrame: A new observations dataframe with an additional QA column. 
+        pd.DataFrame: A new observations dataframe additional QA coluns
     """
-    pass
+    
+    dat = dat.merge(thresholds, on = ["station", "element"], how="left")
+    kwargs = {'variance_df': variance_df} if variance_df else {}
+    
+    for check in checks:
+        dat = check(dat, columns=columns, **kwargs)
+    dat = dat[['station', 'datetime', 'element', 'value', 'qa_range', 'qa_step', 'qa_delta']]
+    return dat
 
+# thresholds = pd.read_csv("../test/elements.csv")
+# thresholds = thresholds[['station', 'element', 'range_min', 'range_max', 'step_size', 'persistence_delta', 'spatial_sd']]
+# dat = pd.read_csv("https://mesonet.climate.umt.edu/api/v2/observations?stations=aceabsar&start_time=2022-10-01&end_time=2022-10-03&wide=False&type=csv&level=0")
+# dat = dat[['station', 'datetime', 'element', 'value']]
+# columns = Columns()
 
+# check_observations(dat, thresholds, columns, ck.check_range_pd, ck.check_step_pd, ck.check_variance_pd)
